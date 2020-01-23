380da860ddc413d40e4fcf780c0a806250f22cb1
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-23 21:37:50 +0900

    add Navigation

--- src/components/VSNavigation.vue
+++ src/components/VSNavigation.vue
@@ -6,59 +6,71 @@
       app
     >
       <v-list dense>
-        <v-btn icon large>
-          <v-avatar size="32px" item>
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-              alt="Vuetify"
-          /></v-avatar>
-        </v-btn>
+        <router-link :to="{ name: 'home' }">
+          <v-btn icon large>
+            <v-avatar size="32px" item>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+                alt="Vuetify"
+            /></v-avatar> </v-btn
+        ></router-link>
         <template v-for="item in items">
-          <v-row v-if="item.heading" :key="item.heading" align="center">
-            <v-col cols="6">
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-col>
-          </v-row>
-          <v-list-group
-            v-else-if="item.children"
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
+          <router-link
+            :to="{ name: item.router }"
+            :key="item.id"
+            class="d-flex text--black"
           >
-            <template v-slot:activator>
+            <!-- headerNav -->
+            <v-row v-if="item.heading" :key="item.heading" align="center">
+              <v-col cols="6">
+                <v-subheader v-if="item.heading">
+                  {{ item.heading }}
+                </v-subheader>
+              </v-col>
+              <v-col cols="6" class="text-center">
+                <a href="#!" class="body-2 black--text">EDIT</a>
+              </v-col>
+            </v-row>
+            <!-- 子Nav -->
+            <v-list-group
+              v-else-if="item.children"
+              :key="item.text"
+              v-model="item.model"
+              :prepend-icon="item.model ? item.icon : item['icon-alt']"
+              append-icon=""
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <!-- 子Nav -->
+              <v-list-item v-for="(child, i) in item.children" :key="i" link>
+                <v-list-item-action v-if="child.icon">
+                  <v-icon>{{ child.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ child.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+            <!-- 普通Nav -->
+
+            <v-list-item v-else :key="item.text">
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>
                   {{ item.text }}
                 </v-list-item-title>
               </v-list-item-content>
-            </template>
-            <v-list-item v-for="(child, i) in item.children" :key="i" link>
-              <v-list-item-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ child.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-          <v-list-item v-else :key="item.text" link>
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.text }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+            </v-list-item></router-link
+          >
         </template>
       </v-list>
     </v-navigation-drawer>
@@ -98,24 +110,20 @@ export default {
     dialog: false,
     drawer: null,
     items: [
-      { icon: "mdi-contacts", text: "Contacts" },
-      { icon: "mdi-history", text: "Frequently contacted" },
+      { icon: "mdi-home", text: "home", router: "home" },
       {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "More",
-        model: false,
-        children: [
-          { text: "Import" },
-          { text: "Export" },
-          { text: "Print" },
-          { text: "Undo changes" },
-          { text: "Other contacts" }
-        ]
+        icon: "mdi-file-document-box-check-outline",
+        text: "form",
+        router: "form"
       }
     ]
   })
 };
 </script>
 
-<style></style>
+<style>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
