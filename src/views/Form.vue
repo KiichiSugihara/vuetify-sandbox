37aa4728b117ddf57ce00f0739f5f92392fdfb42
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-23 21:37:15 +0900

    add Views Form

---
+++ src/views/Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app
+    ><VSNavigation>
+      <template v-slot:titleText>
+        <span>{{ title }}</span>
+      </template></VSNavigation
+    ><v-content>ホーム</v-content></v-app
+  >
+</template>
+
+<script>
+import VSNavigation from "@/components/VSNavigation";
+export default {
+  name: "Home",
+  components: {
+    VSNavigation
+  },
+  data: () => ({
+    title: "フォーム"
+  })
+};
+</script>
