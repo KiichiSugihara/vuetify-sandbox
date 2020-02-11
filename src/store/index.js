05f852d9379f93c94ab23c682f0bff6ee9deabf2
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-11 20:48:14 +0900

    add store auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
-import auth from "@/store/auth";
+import auth from "@/store/module/auth";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export default new Vuex.Store({
   modules: {
     auth
-  }
+  },
+  state: {},
+  mutations: {},
+  actions: {},
+  plugins: [
+    createPersistedState({
+      key: "xxxproject", // プロジェクト単位の一意の識別子
+      paths: ["auth.login"], // auth.js の loginキーは再度アクセスしても保持するようにする
+      storage: localStorage // 今回は localStorage に保存することにする
+    })
+  ]
 });
-
-export default store;
