1981c2efab1a6ed45c30098f5ae22c91ffd6d817
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-13 18:19:38 +0900

    add User api

--- src/views/User.vue
+++ src/views/User.vue
@@ -5,19 +5,62 @@
         <span>{{ title }}</span>
       </template></VSNavigation
     ><v-content class="backGroundColor">
-      <div class="pa-4  white"></div></v-content
-  ></v-app>
+      <div class="pa-4  white">
+        <h1>ユーザーアイテム</h1>
+        <template v-if="loading"
+          ><v-progress-linear
+            :active="loading"
+            :indeterminate="loading"
+            absolute
+            top
+            color="orange"
+          ></v-progress-linear
+        ></template>
+        <template v-else>
+          <span>{{ userItem.email }}</span>
+          <span>{{ userItem.gender }}</span>
+          <template v-if="userItem.name">
+            <span>{{
+              userItem.name.first + userItem.name.last
+            }}</span></template
+          ></template
+        >
+      </div></v-content
+    ></v-app
+  >
 </template>
 
 <script>
 import VSNavigation from "@/components/VSNavigation";
+import axios from "axios";
 export default {
   name: "User",
   components: {
     VSNavigation
   },
   data: () => ({
-    title: "ユーザー"
-  })
+    title: "ユーザー",
+    loading: false,
+    errored: false,
+    error: null,
+    userItem: {}
+  }),
+  async created() {
+    this.loading = true;
+    await this.getItems();
+    this.loading = false;
+  },
+  methods: {
+    getItems() {
+      axios
+        .get("https://randomuser.me/api")
+        .then(response => {
+          this.userItem = response.data.results[0];
+        })
+        .catch(err => {
+          (this.errored = true), (this.error = err);
+        });
+    }
+  }
 };
 </script>
